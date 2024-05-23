253677bb9d7afc958502c6fa396f789269eb4dc3
KAymeric <aymeric.kreuzer@ynov.com>
2024-05-23 21:33:06 +0200

    add menu and restart game

--- Assets/scripts/menu.cs
+++ Assets/scripts/menu.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public GameObject canva;
+    private Custominputs inputActions;
+    private bool isGamePaused = false;
+    private bool isEscToggled = false;
+
+    private void Awake()
     {
-        
+        inputActions = new Custominputs();
+        Time.timeScale = 1.0f;
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
     void Update()
     {
-        
+        pause();
+    }
+
+    private void pause()
+    {
+        if (inputActions.FPSController.pause.ReadValue<float>() > 0)
+        {
+            if (isGamePaused && !isEscToggled)
+            {
+                ContinueGame();
+            }
+            else if (!isEscToggled)
+            {
+                PauseGame();
+            }
+            isEscToggled = true;
+        }
+        else
+        {
+            isEscToggled = false;
+        }
+        canva.SetActive(isGamePaused);
+        Cursor.visible = isGamePaused;
+    }
+
+    private void ContinueGame()
+    {
+        Time.timeScale = 1.0f;
+        isGamePaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        isGamePaused = true;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    public void RestartGame()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
     }
 }
