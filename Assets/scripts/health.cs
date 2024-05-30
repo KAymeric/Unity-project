af7cb32a07b1622f21f343b16b576d90982f39b1
KAymeric <aymeric.kreuzer@ynov.com>
2024-05-30 15:47:42 +0200

    add hit sound and hit particles

--- Assets/scripts/health.cs
+++ Assets/scripts/health.cs
@@ -11,20 +11,25 @@ public class healthManager : MonoBehaviour
     [SerializeField] CharacterController controller;
     [SerializeField] UnityEngine.UI.Slider healthbar;
     [SerializeField] public GameObject canva;
+    [SerializeField] public AudioSource vox;
+    [SerializeField] public ParticleSystem particle;
 
     private float health = 0;
     private float last_velocity = 0;
+    private float particle_cooldown;
 
     private void Start()
     {
         health = base_health;
         healthbar.value = base_health;
         canva.SetActive(false);
+        particle.Stop();
     }
 
     private void Update()
     {
         isTakingFallDamage();
+        updateParticle();
     }
 
     public void update_health(float heal)
@@ -49,9 +54,24 @@ public class healthManager : MonoBehaviour
             if (velo < velocity_resistance)
             {
                 update_health(velo/10);
+                vox.Play();
+                particle.Play();
+                particle_cooldown = 1;
             }
         }
         last_velocity = velo;
+    }
+
+    private void updateParticle()
+    {
+        if (particle_cooldown > 0)
+        {
+            particle_cooldown -= Time.deltaTime;
+        }
+        else
+        {
+            particle.Stop();
+        }
     }
 
     private void Die()
